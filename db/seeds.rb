3db70cd92092a701f1b73f596b51bac3aaf73dc7
vmwhoami <vmwhoami@gmail.com>
2021-03-20 22:04:52 +0200

    Add seeds file to create users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,27 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+names = [
+"Olga Pirvu",
+"Galina Melnic",
+"Filip Melnic",
+"Adam Melnic",
+"Sergiu Bogdan",
+"Inna Bogdan"
+]
+genders = ["male","female"]
+emails = ["vitalimelnic@yahoo.com",
+"vmwhoami@gmail.com","inna@mail.ru",
+"serboss@yahoo.com","adammelnic@gmail.com"
+]
+
+10.times do |el|
+User.create(
+ email: emails.sample(),
+ fullname: names.sample(),
+ gender: genders.sample(),
+ password: "password",
+
+)
+end
+
+puts "***" * 50 
+puts "Users created"
