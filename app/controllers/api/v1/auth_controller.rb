c86e5e9bdc523e13de87db995d58557b1dd9a1c9
vmwhoami <vmwhoami@gmail.com>
2021-03-23 19:45:20 +0200

    First successfull login with postman

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AuthController < ApplicationController
   def login
     user = User.find_by(email: params[:email])
+ 
     if user&.authenticate(params[:password])
       payload = { user_id: user.id }
       token = encode_token(payload)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,28 @@
 class ApplicationController < ActionController::API
- 
   def encode_token(payload)
-    JWT.encode(payload,'adam')
+    JWT.encode(payload, 'adam')
   end
 
   def auth_header
     request.headers['Authorization']
   end
-  
 
-
- def decoded_token
-   if auth_header
-    token = auth_header.split(' ')[1]
- 
-    begin
-      JWT.decode(token,'adam',true, algorithm: 'HS256')
-    rescue => JWT::DecodeError
-      []
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'adam', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        []
+      end
     end
-   end
- end
- 
+  end
 
   def session_user
     decoded_hash = decoded_token
-    if !decoded_hash.empty?
+    unless decoded_hash.empty?
       user_id = decoded_hash[0]['user_id']
       @user = User.find_by(id: user_id)
-    else
-      nil
     end
   end
-  
 end
