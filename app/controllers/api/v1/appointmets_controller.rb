7ddd12a58b0767d747678c4214c2a1938f9260a0
vmwhoami <vmwhoami@gmail.com>
2021-03-25 15:57:31 +0200

    Add delete appointents functionality

--- app/controllers/api/v1/appointmets_controller.rb
+++ app/controllers/api/v1/appointmets_controller.rb
@@ -8,9 +8,22 @@ class Api::V1::AppointmetsController < ApplicationController
 
   def create
     appointment = Appointment.create(user_id: params[:user_id], doctor_id: params[:doctor_id], date_time: params[:time],room: params[:room])
+    if appointment.valid?
     render json: appointment
+    else
+      render json: { errors: appointment.errors.full_messages }, status: :not_acceptable
+    end
   end
 
+  def delete
+    appointment = Appointment.find_by(id: params[:id])
+    if appointment
+      appointment.destroy
+      render json: {success: "Deleted"}
+    else
+      render json: { errors: "Something went wrong" }, status: :not_acceptable
+    end
+  end
   # private
   # def permitted_params
   #   params.permit(:user_id, :doctor_id,:time)
