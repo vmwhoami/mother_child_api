db1c64dad3da698d13791df172fdb4d03ea1fe28
vmwhoami <vmwhoami@gmail.com>
2021-03-31 22:37:53 +0300

    Add regex pattern for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 50 }
+  validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ },
+                    uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
   validates :fullname, presence: true, length: { minimum: 4, maximum: 50 }
   validates :gender, presence: true
   validates :password, presence: true, length: { minimum: 6, maximum: 50 }
